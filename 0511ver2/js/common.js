a6e72e6844d9fc541c428d146c887a79a7502810
小池　浩晃 <hiroakixiaochi847@gmail.com>
2018-05-11 11:55:52 +0900

    ５月１１日の授業

---
+++ 0511ver2/js/common.js
@@ -0,0 +1,32 @@
+console.log('test');
+
+var test = 'test1';
+console.log(test);
+var num = 1;
+console.log(num);
+var num2 = 2;
+console.log(num + num2);
+
+console.log(test + num2);
+
+console.log(1 + 1)
+console.log(1 - 1)
+console.log(2 * 3) //掛け算
+console.log(4 / 2) //割り算
+console.log(9 % 2) //余り
+
+num += 2 //num = num + 2と一緒
+num += 2
+num += 2
+num += 2
+console.log(num)
+num -= 2 //num = num - 2と一緒
+//上と一緒の意味
+num = num + 2
+num = num - 2
+
+
+num++ // num = num + 1と一緒
+num-- // num = num - 1と一緒
+num++
+console.log(num)
