c0de3300eb5159f75dc2f167d283b739fe0a1e0a
小池　浩晃 <hiroakixiaochi847@gmail.com>
2018-06-15 11:58:09 +0900

    0615の授業

---
+++ 0615/js/common.js
@@ -0,0 +1,44 @@
+var num = 100;
+num = 20;
+console.log(num);
+
+//constは再代入ができない
+const num2 = 10;
+//num2 = 30;
+console.log(num2);
+
+const num3 = Math.floor(Math.random() * 10);
+console.log(num3)
+
+//num3の変数が奇数なら、「奇数です」と表示する
+//２で割り切れなかったとき
+
+//自分
+if (num3 % 2 > 0) {　
+    console.log(num3, '奇数です');
+}
+
+//他
+if (num3 % 2 === 1) {　
+    console.log(num3, '奇数です');
+}
+
+if (!(num3 % 2 === 0)) {　
+    console.log(num3, '奇数です');
+}
+
+//num3が奇数の場合は「奇数です」偶数の場合は「偶数です」と表示する
+
+var result = '';
+if (num3 % 2 !== 0) {
+    console.log(num3, '奇数です')
+    result = '奇数です';
+} else if (num3 === 0) {
+    console.log(num3, '０です')
+    result = '０です';
+} else {
+    console.log(num3, '偶数です');
+    result = '偶数です';
+}
+
+$('.box').html(num3 + 'は' + result);
