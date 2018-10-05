2ae6aba602a872e284675ea680cdf047bd5ed2ed
小池　浩晃 <hiroakixiaochi847@gmail.com>
2018-10-05 09:18:43 +0900

    0928の授業のコメントアウトの追加

--- 0928/js/common.js
+++ 0928/js/common.js
@@ -22,7 +22,7 @@ addNum(1, 10) //引数（ひきすう）を渡して実行
 
 //引数に渡された数だけfor文を回す
 function forNumber(num) {
-    for (var i = 0; i < num; i++) {
+    for (var i = 0; i <= num; i++) {
         console.log(i)
     }
 }
@@ -36,10 +36,10 @@ forNumber(10)
 
 // return
 function addNum2(num, num1) {
-    return num + num1
+    return num + num1  //処理
 }
 
-console.log(addNum2(1, 1000)) //処理した結果1001の数字として返ってくる
+console.log(addNum2(1, 1000)) //処理した結果（1001の数字）として返ってくる
 
 
 
@@ -48,12 +48,14 @@ console.log(addNum2(1, 1000)) //処理した結果1001の数字として返っ�
 
 
 //引数で渡した数が入った配列を返す関数
+//指定の数が入った配列をreturnをつかって返す関数
 function numArray(num) {
-    var ary = []
+    var ary = []   //この変数(ary)は「numArray関数」内でしか使えない変数です（ローカル変数）
 
 
     for (var i = 0; i < num; i++) {
         ary[i] = i
+        //ary.push(i)
     }
 
     return ary
@@ -62,3 +64,28 @@ function numArray(num) {
 
 console.log(numArray(20))
 console.log(numArray(50))
+
+//console.log(array)   //arrayが「numArray関数」外で定義（グローバル変数）されてないのでここでエラーになります。
+
+
+
+
+
+
+
+//指定の数が入った配列をreturnを使わないで返す関数?(自分でプログラムを考えたので間違っている可能性あり)
+function numArray2(num2) {
+    var ary2 = []         
+
+
+    for (var i = 0; i < num2; i++) {
+        ary2[i] = i
+        //ary2.push(i)
+    }
+
+    console.log(ary2)
+
+}
+
+numArray2(20)
+numArray2(50)
