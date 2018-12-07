4b9a817d3114ac0e0175c019fec5b793b9f361c0
小池　浩晃 <hiroakixiaochi847@gmail.com>
2018-12-07 12:07:36 +0900

    1207の授業 (1102の授業の続き)

--- 1102/js/common.js
+++ 1102/js/common.js
@@ -1,1 +1,44 @@
+console.log('test')
 
+//jquery_ver (js_verもある)
+
+var tabMenuItem = $('.tabMenuItem')
+var tabMenu = $('.tabItem')
+var prev = $('.prev')
+var next = $('.next')
+
+var currentNum = 0
+
+// 引数num番目のクラスだけonを実行する
+function move(num) {
+
+    //前　次
+
+    currentNum = num
+    console.log(currentNum)
+
+    //Menuとコンテンツ
+
+    //一回onを全て消してからonをつける
+    tabMenuItem.removeClass('on') //クラスを消す
+    tabMenuItem.eq(num).addClass('on') //tabMenuItem.addClass('on')でonをつける。eqは一つづつ取る
+    tabMenu.removeClass('on')
+    tabMenu.eq(num).addClass('on')
+}
+
+tabMenuItem.on('click', function () {
+    //console.log($(this).index()) //クリックした位置のタグの順番を取ってくる
+    move($(this).index()) //引数
+})
+
+prev.on('click', function () {
+    console.log('前', currentNum)
+    //ここで次の値をmove関数に渡してあげる
+    move()
+})
+
+next.on('click', function () {
+    console.log('次', currentNum)
+    //ここで次の値をmove関数に渡してあげる
+    move()
+})
